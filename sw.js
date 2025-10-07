25ea027321b2b656ee3830c56f3a7f1073a2dbc7
vicsanity623 <125237471+vicsanity623@users.noreply.github.com>
2025-10-07 00:43:11 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define the cache name and the files to cache
-const CACHE_NAME = 'vics-td-cache-v1.0.1';
+const CACHE_NAME = 'vics-td-cache-v1.0.2';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
